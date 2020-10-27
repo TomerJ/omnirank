3c6017c8f4d3bd7cec81f7c6752e3fb983745d54
TomerJ <totolavanda@gmail.com>
2020-10-27 18:41:30 +1000

    loading 1

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+const chalk = require("chalk")
 const express = require("express")
 const app = express()
-
-
+const chalk = require("chalk")
+console.log(chalk.green("-----------------"))
+console.log(chalk.green("LOADING OMNIRANK."))
+console.log(chalk.green("-----------------"))
 
 app.listen(8172)
